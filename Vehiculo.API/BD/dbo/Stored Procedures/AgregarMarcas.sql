7cba178f477efa4a9f032b5f23bad5b377ef35be
IndiraRamirez <145410298+IndiraRamirez@users.noreply.github.com>
2025-07-23 18:53:58 -0600

    cambio

--- Vehiculo.API/BD/dbo/Stored Procedures/AgregarMarcas.sql
+++ Vehiculo.API/BD/dbo/Stored Procedures/AgregarMarcas.sql
@@ -3,9 +3,10 @@
 ,@Nombre AS varchar(max)
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- SET NOCOUNT ON added to prevent extra result sets from 
 	-- interfering with SELECT statements.
-	
+    ---- esto es una prueba
+	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
     Begin TRANSACTION
